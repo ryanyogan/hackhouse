bf381594448fc3f221429805f2f6292ba0d52705
ryanyogan <ryanyogan@gmail.com>
2020-11-10 15:47:11 -0600

    basic server apollo

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,29 @@
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import { listings } from './app/fake-api/listings';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/listings', (_req, res) => {
   res.send(listings);
 });
+
+app.post('/delete-listing', (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+
+  return res.send('failed to delted listing');
+});
+
+const apolloServer = new ApolloServer();
+apolloServer.applyMiddleware({ app, path: '/api' });
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
